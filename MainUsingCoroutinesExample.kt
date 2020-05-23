edcffa266483a4322140e64deaa3d948364dc74a
AudiFaathirmansyahMashar <64758987+AudiFaathirmansyahMashar@users.noreply.github.com>
2020-05-23 16:11:45 +0800

    Add files via upload

---
+++ MainUsingCoroutinesExample.kt
@@ -0,0 +1,32 @@
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    launch {
+        val arr = intArrayOf(5,3,6,9,1,2,4,8,7)
+        var temp = 0
+
+        println("Before")
+        arr.forEach {
+            print("${it} ")
+        }
+
+        for (i in 0..arr.size){
+            for (j in 1..i-1){
+                if (arr[j-1] > arr[j]){
+                    temp = arr[j-1]
+                    arr[j-1] = arr[j]
+                    arr[j] = temp
+                }
+            }
+        }
+
+        println("\n==========================")
+        println("After")
+        arr.forEach {
+            print("${it} ")
+        }
+    }
+
+    println("Coroutines Example")
+}
